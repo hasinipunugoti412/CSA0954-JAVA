9cb98d0857138d532de5ca530cac4d11a12d5903
hasinipunugoti412 <114474733+hasinipunugoti412@users.noreply.github.com>
2024-02-02 13:04:13 +0530

    Add files via upload

---
+++ Square_cube.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Square_cube {
+public static void main(String args[]){
+	Scanner input=new Scanner(System.in); 
+	float n=input.nextFloat(); 
+	System.out.println("square: "+(n*n)); 
+	System.out.print("cube: "+(n*n*n));
+}
+}
